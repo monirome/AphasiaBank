d5d552e8cd1faf7f6dc76419b798df16a0945679
monirome <74182452+monirome@users.noreply.github.com>
2022-05-29 11:57:47 +0200

    Update audio_chunks.py

--- audio_chunks.py
+++ audio_chunks.py
@@ -1,3 +1,12 @@
+
+##############################################
+### DEFINE VARIABLES
+### 
+###############################################
+filepath = "XXXX"
+
+################################################
+
 import os
 import glob
 import pandas as pd
@@ -7,12 +16,11 @@ except ImportError:
     from io import StringIO
 # files                                                                         
 
-df = pd.read_csv("C:/Users/Monica/Desktop/.../df_clean.csv") #Change "..." for your directory and the name of the clean dataset
+df = pd.read_csv(filepath) #Change "..." for your directory and the name of the clean dataset
 
 for i in range(len(df)):
-       file = glob.glob(f"""C:/Users/Monica/Desktop/.../{df['file'][i]}""", recursive=True) #change "..." for your audio directory 
+       file = glob.glob(f"""filepath/{df['file'][i]}""", recursive=True) #change "..." for your audio directory 
        start=((pd.to_numeric(df['mark_start'][i]))/1000)
        end=((pd.to_numeric(df['mark_end'][i]))-(pd.to_numeric(df['mark_start'][i])))/1000
        os.system(f"""sox {file[0]} {file[0][:-4]}_{start}_{end}.wav trim {start} {end}""")
-      
-
+     
