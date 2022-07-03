56163855a869678e4dbcb16278a2560da182fdcd
monirome <74182452+monirome@users.noreply.github.com>
2022-07-03 20:20:07 +0200

    Update convert_mp4_to_wav.py

--- convert_mp4_to_wav.py
+++ convert_mp4_to_wav.py
@@ -14,7 +14,7 @@ import glob
 ################################################   
 # Convert mp4 format to wav format 
 
-lst = glob.glob("audiopath/*.mp4", recursive=True) 
+lst = glob.glob(audiopath + "/*.mp4", recursive=True) 
        
 for file in lst:
     os.system(f"""ffmpeg -i {file} -ar 16000 -ac 1 {file[:-4]}.wav""") 
